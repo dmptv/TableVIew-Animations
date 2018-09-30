92882b577528f270b5c0ec9f95e1597ceae5ff96
Kanat <thebesttrois@gmail.com>
2018-09-30 10:57:19 +0600

    c

---
+++ TableVIew-Animations/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  TableVIew-Animations
+//
+//  Created by 123 on 29.09.2018.
+//  Copyright © 2018 kanat. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static var fabviewColor: UIColor {
+        return UIColor(named: "fabview")!
+    }
+    
+    static var fabdarkColor: UIColor {
+        return UIColor(named: "fabdark")!
+    }
+    
+}
